61557f68ff868c9be612c21a913bcf206a0a8c56
kreindo <ahmadsan4p@gmail.com>
2021-01-02 19:52:38 +0800

    first commit

---
+++ isp.py
@@ -0,0 +1,8 @@
+from alphaemOnline import isekai
+
+def isekaiPreventionSystem():
+    while True:
+        if isekai(isekaiPreventionSystem) == True:
+            return preventSys()
+        else:
+            return
